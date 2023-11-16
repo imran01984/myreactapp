3be82639be7458d7e00068bf4786f57a080b1266
Imran Alam <imran.alam@accenture.com>
2023-11-16 10:21:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import OpenApi from './components/OpenApi';
+import ChatGpt from './components/Images/ChatGpt-Icon.jpg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <Navbar title="React App" about="About us"/>  <br/>
+    <div> 
+    <h1>Welcome to ChatGPT App</h1>    
+    <img src={ChatGpt} alt='ChatGpt'  width="550" height="200"/>    
+    <OpenApi />
     </div>
-  );
+    </>
+  ); 
 }
 
 export default App;
---
+++ src/components/OpenApi.js
@@ -0,0 +1,92 @@
+import { useState }  from 'react';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import {  MainContainer,  ChatContainer,  MessageList,  Message,  MessageInput,  TypingIndicator, } from '@chatscope/chat-ui-kit-react';
+
+
+export default function OpenApi() {
+
+    const API_KEY ="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"
+    
+    const [messages, setMessages] = useState([
+          {
+            message: "Hello, I'm ChatGPT! Ask me anything!",
+            sentTime: "just now",
+            sender: "ChatGPT",
+          },
+        ]);
+        const [isTyping, setIsTyping] = useState(false);
+      
+        const handleSendRequest = async (message) => {
+          const newMessage = {
+            message,
+            direction: 'outgoing',
+            sender: "user",
+          };
+      
+          setMessages((prevMessages) => [...prevMessages, newMessage]);
+          setIsTyping(true);
+      
+          try {
+            const response = await processMessageToChatGPT([...messages, newMessage]);
+            const content = response.choices[0]?.message?.content;
+            if (content) {
+              const chatGPTResponse = {
+                message: content,
+                sender: "ChatGPT",
+              };
+              setMessages((prevMessages) => [...prevMessages, chatGPTResponse]);
+            }
+          } catch (error) {
+            console.error("Error processing message:", error);
+          } finally {
+            setIsTyping(false);
+          }
+        };
+      
+        async function processMessageToChatGPT(chatMessages) {
+          const apiMessages = chatMessages.map((messageObject) => {
+            const role = messageObject.sender === "ChatGPT" ? "assistant" : "user";
+            return { role, content: messageObject.message };
+          });      
+          const apiRequestBody = {
+            "model": "gpt-3.5-turbo",
+            "messages": [
+              { role: "system", 
+              content: "I'm a OpenAI User using ChatGPT for learning" 
+              },
+              ...apiMessages,
+            ],
+          };      
+          const response = await fetch("https://api.openai.com/v1/chat/completions", {
+            method: "POST",
+            headers: {
+              "Authorization": "Bearer " + API_KEY,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(apiRequestBody),
+          });
+      
+          return response.json();
+        }
+  return (
+    <div className="App">
+      <div style={{ position:"absolute", height: "600px", width: "550px", border: "3px solid #73AD21" }}>      
+        <MainContainer>
+          <ChatContainer>                 
+            <MessageList 
+              scrollBehavior="smooth" 
+              typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}>
+              
+              {messages.map((message, i) => {
+                console.log(message)
+                return <Message key={i} model={message} />
+              })}
+
+            </MessageList>
+            <MessageInput placeholder="Send a Message" onSend={handleSendRequest} />        
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </div>
+  )
+}
